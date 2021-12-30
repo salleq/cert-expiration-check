cf8b43ffe7a3055dcc4079e9984033e827bdae57
salleq <1240177+salleq@users.noreply.github.com>
2021-12-30 10:29:28 +0200

    Update certcheck.sh
    added option to print output in either json or plain text

--- certcheck.sh
+++ certcheck.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-
-expiration (){
-expiration=$(echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -inform pem -noout -text | grep 'Not After' | sed 's/Not After : //')
-future=$(date +%s --date "$expiration")
-difference=$(($future-$now))
-echo -e $1 '\t\t'$(($difference/(3600*24))) days
-}
+format=$1
+if [ -z "$1" ]
+then format="-json"
+fi
 
 now=$(date +%s)
+
+expiration (){
+expiration=$(echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -inform pem -noout -text | grep 'Not After' | sed 's/Not Afte>future=$(date +%s --date "$expiration")
+difference=$(($future-$now))
+
+if [[ $format = "-json" ]];
+then
+echo -e '{"domain":"'$1'","days":"'$(($difference/(3600*24)))'"}'
+
+elif [[ $format = "-txt" ]];
+then
+echo -e $1'\t'$(($difference/(3600*24)))
+fi
+
+}
 
 #enter any wanted domains here like below
 expiration www.google.com
