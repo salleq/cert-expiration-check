b1b4985aab3e1ddb04934a59fa21676a677b1695
salleq <1240177+salleq@users.noreply.github.com>
2021-12-30 11:02:55 +0200

    Update certcheck.sh
    Switched domain list to an array to be read from another filer

--- certcheck.sh
+++ certcheck.sh
@@ -6,23 +6,32 @@ fi
 
 now=$(date +%s)
 
-expiration (){
-expiration=$(echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -inform pem -noout -text | grep 'Not After' | sed 's/Not Afte>future=$(date +%s --date "$expiration")
+# If you want to list domains in the script instead of external file, comment rows 15-19 and uncomment rows 10,14 and add 
+# domains between those rows
+
+# domains=(
+# #####list domains here one per row
+
+# ######
+# )
+readarray -t domains < ./cert-domains
+
+for i in "${domains[@]}"
+
+do
+
+expiration=$(echo | openssl s_client -showcerts -servername $i -connect $i:443 2>/dev/null | openssl x509 -inform pem -noout -text | grep 'Not After' | sed 's/Not After : //')
+future=$(date +%s --date "$expiration")
 difference=$(($future-$now))
 
 if [[ $format = "-json" ]];
 then
-echo -e '{"domain":"'$1'","days":"'$(($difference/(3600*24)))'"}'
+echo -e '{"domain":"'$i'","days":"'$(($difference/(3600*24)))'"}'
 
 elif [[ $format = "-txt" ]];
 then
-echo -e $1'\t'$(($difference/(3600*24)))
+echo -e $i'\t'$(($difference/(3600*24)))
+
 fi
 
-}
-
-#enter any wanted domains here like below
-expiration www.google.com
-expiration www.microsoft.com
-
-
+done
